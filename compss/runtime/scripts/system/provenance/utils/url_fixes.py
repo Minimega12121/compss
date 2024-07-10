1a4f87315e1b99c3ac0e4c6dba253c0b19d345e8
rsirvent <iceroach@gmail.com>
2024-07-10 17:47:22 +0200

    Refactoring of create_action

--- compss/runtime/scripts/system/provenance/utils/url_fixes.py
+++ compss/runtime/scripts/system/provenance/utils/url_fixes.py
@@ -22,3 +22,55 @@ def fix_dir_url(in_url: str) -> str:
         return new_url
     # else
     return in_url  # No changes required
+
+
+def fix_in_files_at_out_dirs(
+    inputs_list: list, outputs_list: list
+) -> typing.Tuple[list, list]:
+    """
+    Remove any file inputs that the user may have declared as directory outputs
+
+    :param inputs_list: list of all input directories and files as URLs
+    :param compss_wf_info: list of all output directories and files as URLs
+
+
+    :returns: Updated inputs and outputs lists
+    """
+
+    # Now erase any file:// input that is included as dir:// output
+    directories_list = []
+    file_found = False
+    for item in outputs_list:
+        url_parts = urlsplit(item)
+        if url_parts.scheme == "dir":
+            if url_parts.path not in directories_list:
+                directories_list.append(url_parts.path)
+        else:
+            file_found = True
+            break
+
+    i = 0
+    file_found = False
+    for item in inputs_list:
+        url_parts = urlsplit(item)
+        if url_parts.scheme == "dir":
+            i += 1
+        else:
+            file_found = True
+            break
+
+    if not file_found:
+        return inputs_list, outputs_list
+
+    url_files_list = inputs_list[i:]  # Slice out directories
+    for item in url_files_list:
+        url_parts = urlsplit(item)
+        if any(url_parts.path.startswith(dir_path) for dir_path in directories_list):
+            print(
+                f"PROVENANCE | WARNING: Metadata of an input file has been removed since it is included at an output directory: {url_parts.path}"
+            )
+            inputs_list.remove(item)
+
+    # print(f"PROVENANCE DEBUG | RESULT FROM fix_in_files_at_out_dirs:\n {inputs_list}")
+
+    return inputs_list, outputs_list
---
+++ compss/runtime/scripts/system/provenance/wrroc/create_action.py
@@ -0,0 +1,298 @@
+import typing
+import os
+import uuid
+import subprocess
+import socket
+
+from pathlib import Path
+from datetime import timezone
+from datetime import datetime
+# from ..utils.url_fixes import fix_dir_url
+# TODO: FIX SIBLING PACKAGE REFERENCE
+
+from rocrate.rocrate import ROCrate
+from rocrate.model.person import Person
+from rocrate.model.contextentity import ContextEntity
+from rocrate.utils import iso_now
+
+
+# TODO: FIX SIBLING PACKAGE REFERENCE
+def fix_dir_url(in_url: str) -> str:
+    """
+    Fix dir:// URL returned by the runtime, change it to file:// and ensure it ends with '/'
+
+    :param in_url: URL that may need to be fixed
+
+    :returns: A file:// URL
+    """
+
+    from urllib.parse import urlsplit
+
+    runtime_url = urlsplit(in_url)
+    if (
+        runtime_url.scheme == "dir"
+    ):  # Fix dir:// to file:// and ensure it ends with a slash
+        new_url = "file://" + runtime_url.netloc + runtime_url.path
+        if new_url[-1] != "/":
+            new_url += "/"  # Add end slash if needed
+        return new_url
+    # else
+    return in_url  # No changes required
+
+# TODO: FIX SIBLING PACKAGE REFERENCE
+def add_person_definition(
+    compss_crate: ROCrate,
+    contact_type: str,
+    yaml_author: dict,
+    info_yaml: str
+) -> bool:
+    """
+    Check if a specified person has enough defined terms to be added in the RO-Crate.
+
+    :param compss_crate: The COMPSs RO-Crate being generated
+    :param contact_type: contactType definition for the ContactPoint. "Author" or "Agent"
+    :param yaml_author: Content of the YAML file describing the user
+    :param info_yaml: Name of the YAML file specified by the user
+
+    :returns: If the person is valid. It is added if True
+    """
+
+    # Expected Person fields
+    # orcid - Mandatory in RO-Crate 1.1
+    # name - Mandatory in WorkflowHub
+    # e-mail - Optional
+    #
+    # ror - Optional
+    # organisation_name - Optional even if ror is defined. But won't show anything at WorkflowHub
+
+    person_dict = {}
+    mail_dict = {}
+    org_dict = {}
+
+    if not "orcid" in yaml_author:
+        print(
+            f"PROVENANCE | ERROR in your {info_yaml} file. A 'Person' is ignored, since it has no 'orcid' defined"
+        )
+        return False
+    if "name" in yaml_author:
+        person_dict["name"] = yaml_author["name"]
+    # Name is no longer mandatory in WFHub
+    # else:
+    #     print(f"PROVENANCE | ERROR in your {info_yaml} file. A 'Person' is ignored, since it has 'orcid' but no 'name' defined")
+    #     return False
+    if "e-mail" in yaml_author:
+        person_dict["contactPoint"] = {"@id": "mailto:" + yaml_author["e-mail"]}
+        mail_dict["@type"] = "ContactPoint"
+        mail_dict["contactType"] = contact_type
+        mail_dict["email"] = yaml_author["e-mail"]
+        mail_dict["identifier"] = yaml_author["e-mail"]
+        mail_dict["url"] = yaml_author["orcid"]
+        compss_crate.add(
+            ContextEntity(compss_crate, "mailto:" + yaml_author["e-mail"], mail_dict)
+        )
+    if "ror" in yaml_author:
+        person_dict["affiliation"] = {"@id": yaml_author["ror"]}
+        # If ror defined, organisation_name becomes mandatory, if it is to be shown in WorkflowHub
+        org_dict["@type"] = "Organization"
+        if "organisation_name" in yaml_author:
+            org_dict["name"] = yaml_author["organisation_name"]
+            compss_crate.add(ContextEntity(compss_crate, yaml_author["ror"], org_dict))
+        else:
+            print(
+                f"PROVENANCE | WARNING in your {info_yaml} file. 'organisation_name' not defined for an 'Organisation'"
+            )
+    compss_crate.add(Person(compss_crate, yaml_author["orcid"], person_dict))
+
+    return True
+
+
+def wrroc_create_action(
+    compss_crate: ROCrate,
+    main_entity: str,
+    author_list: list,
+    ins: list,
+    outs: list,
+    yaml_content: dict,
+    info_yaml: str,
+    dp_log: str
+) -> str:
+    """
+    Add a CreateAction term to the ROCrate to make it compliant with WRROC.  RO-Crate WorkflowRun Level 2 profile,
+    aka. Workflow Run Crate.
+
+    :param compss_crate: The COMPSs RO-Crate being generated
+    :param main_entity: The name of the source file that contains the COMPSs application main() method
+    :param author_list: List of authors as described in the YAML
+    :param ins: List of input files of the workflow
+    :param outs: List of output files of the workflow
+    :param yaml_content: Content of the YAML file specified by the user
+    :param info_yaml: Name of the YAML file specified by the user
+    :param dp_log: Full path to the dataprovenance.log file
+
+    :returns: UUID generated for this run
+    """
+
+    # Compliance with RO-Crate WorkflowRun Level 2 profile, aka. Workflow Run Crate
+    # marenostrum4, nord3, ... BSC_MACHINE would also work
+    host_name = os.getenv("SLURM_CLUSTER_NAME")
+    if host_name is None:
+        host_name = os.getenv("BSC_MACHINE")
+        if host_name is None:
+            host_name = socket.gethostname()
+    job_id = os.getenv("SLURM_JOB_ID")
+
+    main_entity_pathobj = Path(main_entity)
+
+    run_uuid = str(uuid.uuid4())
+
+    if job_id is None:
+        name_property = (
+            "COMPSs " + main_entity_pathobj.name + " execution at " + host_name
+        )
+        userportal_url = None
+        create_action_id = "#COMPSs_Workflow_Run_Crate_" + host_name + "_" + run_uuid
+    else:
+        name_property = (
+            "COMPSs "
+            + main_entity_pathobj.name
+            + " execution at "
+            + host_name
+            + " with JOB_ID "
+            + job_id
+        )
+        userportal_url = "https://userportal.bsc.es/"  # job_id cannot be added, does not match the one in userportal
+        create_action_id = (
+            "#COMPSs_Workflow_Run_Crate_" + host_name + "_SLURM_JOB_ID_" + job_id
+        )
+    compss_crate.root_dataset["mentions"] = {"@id": create_action_id}
+
+    # OSTYPE, HOSTTYPE, HOSTNAME defined by bash and not inherited. Changed to "uname -a"
+    uname = subprocess.run(["uname", "-a"], stdout=subprocess.PIPE, check=True)
+    uname_out = uname.stdout.decode("utf-8")[:-1]  # Remove final '\n'
+
+    # SLURM interesting variables: SLURM_JOB_NAME, SLURM_JOB_QOS, SLURM_JOB_USER, SLURM_SUBMIT_DIR, SLURM_NNODES or
+    # SLURM_JOB_NUM_NODES, SLURM_JOB_CPUS_PER_NODE, SLURM_MEM_PER_CPU, SLURM_JOB_NODELIST or SLURM_NODELIST.
+
+    environment_property = []
+    for name, value in os.environ.items():
+        if (
+            name.startswith(("SLURM_JOB", "SLURM_MEM", "SLURM_SUBMIT", "COMPSS"))
+            and name != "SLURM_JOBID"
+        ):
+            # Changed to 'environment' term in WRROC v0.4
+            env_var = {}
+            env_var["@type"] = "PropertyValue"
+            env_var["name"] = name
+            env_var["value"] = value
+            compss_crate.add(
+                ContextEntity(
+                    compss_crate,
+                    "#" + name.lower(),
+                    properties=env_var,
+                )
+            )
+            environment_property.append({"@id": "#" + name.lower()})
+
+    description_property = uname_out
+
+    resolved_main_entity = main_entity
+    for entity in compss_crate.get_entities():
+        if "ComputationalWorkflow" in entity.type:
+            resolved_main_entity = entity.id
+
+    # Register user submitting the workflow
+    if "Agent" in yaml_content and add_person_definition(
+        compss_crate, "Agent", yaml_content["Agent"], info_yaml
+    ):
+        agent = {"@id": yaml_content["Agent"]["orcid"]}
+    else:  # Choose first author, to avoid leaving it empty. May be true most of the times
+        if author_list:
+            agent = author_list[0]
+            print(
+                f"PROVENANCE | WARNING: 'Agent' not specified in {info_yaml}. First author selected by default."
+            )
+        else:
+            agent = None
+            print(
+                f"PROVENANCE | WARNING: No 'Authors' or 'Agent' specified in {info_yaml}"
+            )
+
+    create_action_properties = {
+        "@type": "CreateAction",
+        "instrument": {"@id": resolved_main_entity},  # Resolved path of the main file
+        "actionStatus": {"@id": "http://schema.org/CompletedActionStatus"},
+        "endTime": iso_now(),  # Get current time
+        "name": name_property,
+        "description": description_property,
+    }
+    if len(environment_property) > 0:
+        create_action_properties["environment"] = environment_property
+
+    if job_id:
+        sacct_command = ["sacct", "-j", str(job_id), "--format=Start", "--noheader"]
+        head_command = ["head", "-n", "1"]
+        sacct_process = subprocess.Popen(sacct_command, stdout=subprocess.PIPE)
+        head_process = subprocess.Popen(
+            head_command, stdin=sacct_process.stdout, stdout=subprocess.PIPE
+        )
+        output, _ = head_process.communicate()
+        start_time_str = output.decode("utf-8").strip()
+        # Convert start time to datetime object
+        start_time = datetime.strptime(start_time_str, "%Y-%m-%dT%H:%M:%S")
+        create_action_properties["startTime"] = start_time.astimezone(
+            timezone.utc
+        ).isoformat()
+    else:
+        # Take startTime from dataprovenance.log when no queuing system is involved
+        # The string generated by the runtime is already in UTC
+        with open(dp_log, "r", encoding="UTF-8") as dp_file:
+            for i, line in enumerate(dp_file):
+                if i == 3:
+                    start_time = datetime.strptime(
+                        line.strip(), "%Y-%m-%dT%H:%M:%S.%f%z"
+                    )
+                    create_action_properties["startTime"] = start_time.replace(
+                        microsecond=0
+                    ).isoformat()
+                    break
+
+    if agent:
+        create_action_properties["agent"] = agent
+
+    create_action = compss_crate.add(
+        ContextEntity(compss_crate, create_action_id, create_action_properties)
+    )  # id can be something fancy for MN4, otherwise, whatever
+    create_action.properties()
+
+    # "subjectOf": {"@id": userportal_url}
+    if userportal_url is not None:
+        create_action.append_to("subjectOf", userportal_url)
+
+    # "object": [{"@id":}],  # List of inputs
+    # "result": [{"@id":}]  # List of outputs
+    # Right now neither the COMPSs runtime nor this script check if a file URI is inside a dir URI. This means
+    # duplicated entries can be found in the metadata (i.e. a file that is part of a directory, can be added
+    # independently). However, this does not add duplicated files if data_persistence is True
+    # Hint for controlling duplicates: both 'ins' and 'outs' dir URIs come first on each list
+    for item in ins:
+        create_action.append_to("object", {"@id": fix_dir_url(item)})
+    for item in outs:
+        create_action.append_to("result", {"@id": fix_dir_url(item)})
+    create_action.append_to("result", {"@id": "./"})  # The generated RO-Crate
+
+    # Add out and err logs in SLURM executions
+    if job_id:
+        suffix = [".out", ".err"]
+        msg = ["output", "error"]
+        for f_suffix, f_msg in zip(suffix, msg):
+            file_properties = {}
+            file_properties["name"] = "compss-" + job_id + f_suffix
+            file_properties["contentSize"] = os.path.getsize(file_properties["name"])
+            file_properties["description"] = (
+                "COMPSs console standard " + f_msg + " log file"
+            )
+            file_properties["encodingFormat"] = "text/plain"
+            file_properties["about"] = create_action_id
+            compss_crate.add_file(file_properties["name"], properties=file_properties)
+
+    return run_uuid
