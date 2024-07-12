cc32ce05c1db68589695b40a51ef3d8466e15081
fconejer <francisco.conejero@bsc.es>
2024-07-12 16:49:18 +0200

    Improved redirection to avoid issues using EAR or other tools

--- compss/programming_model/bindings/python/src/pycompss/util/std/redirects.py
+++ compss/programming_model/bindings/python/src/pycompss/util/std/redirects.py
@@ -26,6 +26,7 @@ or child processes).
 """
 
 import ctypes
+import fcntl
 import io
 import os
 import sys
@@ -40,6 +41,45 @@ if sys.platform == "darwin":
 else:
     C_STDOUT = ctypes.c_void_p.in_dll(LIBC, "stdout")
     C_STDERR = ctypes.c_void_p.in_dll(LIBC, "stderr")
+
+
+class FDLocker:
+    """File descriptor locker.
+
+    This class implements a context able to lock a file in order to ensure
+    that the access to it is done in exclusion.
+    """
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file_descriptor = open(file_name, "ab")
+
+    def __enter__ (self):
+        if self.file_descriptor.closed:
+            self.file_descriptor = open(self.file_name, "ab")
+        fcntl.flock(self.file_descriptor.fileno(), fcntl.LOCK_EX)
+        return self
+
+    def __exit__ (self, _type, value, tb):
+        self.file_descriptor.flush()
+        fcntl.flock(self.file_descriptor.fileno(), fcntl.LOCK_UN)
+        self.file_descriptor.close()
+
+
+def _dup2(to_fd: int, from_fd: int) -> None:
+    """Wrapper around dup2 to do retries if fails.
+
+    :param to_fd: Destination file descriptor.
+    :param from_fd: Source file descriptor.
+    :return: None.
+    """
+    retries = 5
+    while retries > 0:
+        try:
+            os.dup2(to_fd, from_fd)
+            return
+        except OSError:
+            retries = retries - 1
 
 
 @contextmanager
@@ -64,8 +104,18 @@ def std_redirector(
     :param err_filename: Error output file filename (where to redirect stderr)
     :return: Generator
     """
-    stdout_fd = sys.stdout.fileno()
-    stderr_fd = sys.stderr.fileno()
+    stdout = sys.stdout
+    stderr = sys.stderr
+    try:
+        stdout_fd = stdout.fileno()
+    except ValueError:
+        sys.stdout = os.fdopen(1, 'w') #, 0)
+        stdout_fd = sys.stdout.fileno()
+    try:
+        stderr_fd = stderr.fileno()
+    except ValueError:
+        sys.stderr = os.fdopen(2, 'w') #, 0)
+        stderr_fd = sys.stderr.fileno()
 
     def _redirect_stdout(to_fd: int) -> None:
         """Redirect stdout to the given file descriptor.
@@ -76,9 +126,10 @@ def std_redirector(
         # Flush the C-level buffer stdout
         LIBC.fflush(C_STDOUT)
         # Flush and close sys.stdout (also closes the file descriptor)
+        sys.stdout.flush()
         sys.stdout.close()
         # Make stdout_fd point to_fd
-        os.dup2(to_fd, stdout_fd)
+        _dup2(to_fd, stdout_fd)
         # Create a new sys.stdout that points to the redirected fd
         sys.stdout = io.TextIOWrapper(os.fdopen(stdout_fd, "wb"))
 
@@ -91,9 +142,10 @@ def std_redirector(
         # Flush the C-level buffer stderr
         LIBC.fflush(C_STDERR)
         # Flush and close sys.stderr (also closes the file descriptor)
+        sys.stderr.flush()
         sys.stderr.close()
         # Make stderr_fd point to_fd
-        os.dup2(to_fd, stderr_fd)
+        _dup2(to_fd, stderr_fd)
         # Create a new sys.stderr that points to the redirected fd
         sys.stderr = io.TextIOWrapper(os.fdopen(stderr_fd, "wb"))
 
@@ -101,22 +153,21 @@ def std_redirector(
     stdout_fd_backup = os.dup(stdout_fd)
     stderr_fd_backup = os.dup(stderr_fd)
 
-    f_out = open(out_filename, "ab")
-    _redirect_stdout(f_out.fileno())
-    f_err = open(err_filename, "ab")
-    _redirect_stderr(f_err.fileno())
+    with FDLocker(out_filename) as f_out:
+        _redirect_stdout(f_out.file_descriptor.fileno())
+    with FDLocker(err_filename) as f_err:
+        _redirect_stderr(f_err.file_descriptor.fileno())
 
     # Yield to caller
     yield
 
     # Then redirect stdout and stderr back to the backup file descriptors
-    _redirect_stdout(stdout_fd_backup)
-    f_out.flush()
-    _redirect_stderr(stderr_fd_backup)
-    f_err.flush()
-    f_out.close()
+    with FDLocker(out_filename) as f_out:
+        _redirect_stdout(stdout_fd_backup)
+    with FDLocker(err_filename) as f_err:
+        _redirect_stderr(stderr_fd_backup)
+    # Close file descriptors
     os.close(stdout_fd_backup)
-    f_err.close()
     os.close(stderr_fd_backup)
 
 
@@ -130,8 +181,18 @@ def ipython_std_redirector(
     :param err_filename: Error output file filename (where to redirect stderr)
     :return: Generator
     """
-    stdout_fd = sys.__stdout__.fileno()
-    stderr_fd = sys.__stderr__.fileno()
+    stdout = sys.__stdout__
+    stderr = sys.__stderr__
+    try:
+        stdout_fd = stdout.fileno()
+    except ValueError:
+        sys.stdout = os.fdopen(1, 'w')  # , 0)
+        stdout_fd = sys.stdout.fileno()
+    try:
+        stderr_fd = stderr.fileno()
+    except ValueError:
+        sys.stderr = os.fdopen(2, 'w')  # , 0)
+        stderr_fd = sys.stderr.fileno()
 
     def _redirect_stdout(to_fd: int) -> None:
         """Redirect stdout to the given file descriptor.
@@ -142,10 +203,12 @@ def ipython_std_redirector(
         # Flush the C-level buffer stdout
         LIBC.fflush(C_STDOUT)
         # Flush and close sys.__stdout__ (also closes the file descriptor)
+        sys.__stdout__.flush()
         sys.__stdout__.close()
+        sys.stdout.flush()
         sys.stdout.close()
         # Make stdout_fd point to_fd
-        os.dup2(to_fd, stdout_fd)
+        _dup2(to_fd, stdout_fd)
         # Create a new sys.__stdout__ that points to the redirected fd
         new_out = io.TextIOWrapper(os.fdopen(stdout_fd, "wb"))
         sys.__stdout__ = new_out  # type: ignore
@@ -160,10 +223,12 @@ def ipython_std_redirector(
         # Flush the C-level buffer stderr
         LIBC.fflush(C_STDERR)
         # Flush and close sys.__stderr__ (also closes the file descriptor)
+        sys.__stderr__.flush()
         sys.__stderr__.close()
+        sys.stderr.flush()
         sys.stderr.close()
         # Make stderr_fd point to_fd
-        os.dup2(to_fd, stderr_fd)
+        _dup2(to_fd, stderr_fd)
         # Create a new sys.__stderr__ that points to the redirected fd
         new_err = io.TextIOWrapper(os.fdopen(stderr_fd, "wb"))
         sys.__stderr__ = new_err  # type: ignore
@@ -172,19 +237,20 @@ def ipython_std_redirector(
     # Save a copy of the original stdout and stderr
     stdout_fd_backup = os.dup(stdout_fd)
     stderr_fd_backup = os.dup(stderr_fd)
-    f_out = open(out_filename, "ab")
-    _redirect_stdout(f_out.fileno())
-    f_err = open(err_filename, "ab")
-    _redirect_stderr(f_err.fileno())
+
+    with FDLocker(out_filename) as f_out:
+        _redirect_stdout(f_out.file_descriptor.fileno())
+    with FDLocker(err_filename) as f_err:
+        _redirect_stderr(f_err.file_descriptor.fileno())
+
     # Yield to caller
     yield
+
     # Then redirect stdout and stderr back to the backup file descriptors
-    _redirect_stdout(stdout_fd_backup)
-    f_out.flush()
-    _redirect_stderr(stderr_fd_backup)
-    f_err.flush()
+    with FDLocker(out_filename) as f_out:
+        _redirect_stdout(stdout_fd_backup)
+    with FDLocker(err_filename) as f_err:
+        _redirect_stderr(stderr_fd_backup)
     # Close file descriptors
-    f_out.close()
     os.close(stdout_fd_backup)
-    f_err.close()
     os.close(stderr_fd_backup)
